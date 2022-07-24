7b00309c121da0fb7e680688b287052d3ffc124e
Callum Bullard <mail@cbullard.com>
2022-07-24 15:29:19 +1000

    Added new views directory for AppHome, Modals and Messages Added a new router for slack Added a new helper class for all helper functions Created app home for with API token and without

---
+++ src/slack.js
@@ -0,0 +1,80 @@
+const express = require('express')
+const router = express.Router()
+const ax = require('axios')
+const { appHome, appHomeNoToken } = require('../views/home')
+const helper = require('./helpers')
+
+const slack_api = ax.create({
+  baseURL: 'https://slack.com/api',
+  headers: { Authorization: 'Bearer ' + process.env.SLACK_BOT_TOKEN },
+})
+
+const dummy_weather = {
+  coord: {
+    lon: -122.08,
+    lat: 37.39,
+  },
+  weather: [
+    {
+      id: 800,
+      main: 'Clear',
+      description: 'clear sky',
+      icon: '01d',
+    },
+  ],
+  base: 'stations',
+  main: {
+    temp: 282.55,
+    feels_like: 281.86,
+    temp_min: 280.37,
+    temp_max: 284.26,
+    pressure: 1023,
+    humidity: 100,
+  },
+  visibility: 10000,
+  wind: {
+    speed: 1.5,
+    deg: 350,
+  },
+  clouds: {
+    all: 1,
+  },
+  dt: 1560350645,
+  sys: {
+    type: 1,
+    id: 5122,
+    message: 0.0139,
+    country: 'US',
+    sunrise: 1560343627,
+    sunset: 1560396563,
+  },
+  timezone: -25200,
+  id: 420006353,
+  name: 'Mountain View',
+  cod: 200,
+  emoji: '',
+}
+dummy_weather.emoji = helper(dummy_weather.weather[0].description)
+
+router.post('/events', async (req, res) => {
+  if (req.body.challenge) res.send(req.body.challenge)
+  if (req.body.event.type === 'app_home_opened') {
+    res.sendStatus(200).end()
+    try {
+      const body = {
+        user_id: req.body.event.user,
+        view: appHome(undefined, dummy_weather, undefined),
+      }
+      console.log(body)
+      const response = await slack_api.post('/views.publish', body)
+      console.log(response.data)
+    } catch (e) {
+      console.error(e.data)
+    }
+    return
+  }
+  res.send(200).end()
+  console.log(req.body)
+})
+
+module.exports = router
---
+++ views/home.js
@@ -0,0 +1,63 @@
+const { HomeTab, Blocks, Elements, Section } = require('slack-block-builder')
+
+const appHomeNoToken = () => {
+  return HomeTab()
+    .blocks(
+      Blocks.Header({ text: ':mostly_sunny: Simple Weather :mostly_sunny:' }),
+      Blocks.Section({
+        text: 'This is a simple weather app using the Open Weather Map API',
+      }),
+      Blocks.Divider(),
+      Blocks.Section({
+        text: `Doesn't look like you have added an Open Weather Map API token yet.\nClick the settings button to setup.`,
+      }),
+      Blocks.Actions().elements(
+        Elements.Button({
+          type: 'plain_text',
+          text: ':gear: Settings',
+          emoji: true,
+        })
+          .actionId('open_settings')
+          .primary()
+      )
+    )
+    .buildToObject()
+}
+
+const appHome = (location, todays_weather, five_day_forecast) => {
+  return HomeTab()
+    .blocks(
+      Blocks.Header({ text: ':mostly_sunny: Simple Weather :mostly_sunny:' }),
+      Blocks.Section({
+        text: 'This is a simple weather app using the Open Weather Map API',
+      }),
+      Blocks.Divider(),
+      Blocks.Header({
+        text: `${todays_weather.emoji} Today's Weather for ${todays_weather.name}`,
+      }),
+      Blocks.Section().fields(
+        `Weather: ${todays_weather.weather[0].description}`,
+        `Temp: ${todays_weather.main.temp}`,
+        `Min temp: ${todays_weather.main.temp_min}`,
+        `Max temp: ${todays_weather.main.temp_max}`,
+        `Feels like: ${todays_weather.main.feels_like}`
+      ),
+      Blocks.Divider(),
+      Blocks.Header({ text: `Settings` }),
+      Blocks.Actions().elements(
+        Elements.Button({
+          type: 'plain_text',
+          text: ':gear: Settings',
+          emoji: true,
+        })
+          .actionId('open_settings')
+          .primary()
+      )
+    )
+    .buildToObject()
+}
+
+module.exports = {
+  appHomeNoToken,
+  appHome,
+}
